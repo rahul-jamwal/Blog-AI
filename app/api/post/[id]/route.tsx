7a1b0c79330dc27672de0f2807356213a41c7c97
Rahul Jamwal <therahulj9@gmail.com>
2023-06-20 17:16:05 +0530

    First commit

---
+++ app/api/post/[id]/route.tsx
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../client";
+
+type Params = {params: {id: string}}
+
+export async function PATCH(request: Request, { params }: Params){
+    try{
+        const { id } = params;
+        const { title, content } = await request.json(); 
+        const post = await prisma.post.update({
+            where: {id: id},
+            data: {title, content}
+        })
+
+        return NextResponse.json(post, {status: 200})
+    }
+    catch(err){
+        console.error("request error", err)
+        NextResponse.json({ err: "error updating post"}, {status: 500});
+    }
+}
---
+++ app/types.ts
@@ -0,0 +1,12 @@
+export type FormattedPost = {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    title: string;
+    category: string;
+    content: string;
+    author: string;
+    image: string;
+    snippet: string;
+  };
+  
